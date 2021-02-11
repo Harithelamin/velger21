68a0289be331d7804bd9f647e7526b8578013fcb
harith elamin <harithelamin@gmail.com>
2021-02-11 10:27:46 +0100

    New Spring Boog Project

---
+++ src/main/java/com/velger/Velgerguide2021/Velgerguide2021Application.java
@@ -0,0 +1,13 @@
+package com.velger.Velgerguide2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Velgerguide2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Velgerguide2021Application.class, args);
+	}
+
+}
