ae23cf5b2bf47db3e8f7806c42eac11d2dc6e1a5
Kim <56063627+kimpal@users.noreply.github.com>
2021-04-22 17:43:25 +0200

    endret på Barchart gjort coden mer lesbar

--- velgerguideClient/src/app/bar/bar.component.ts
+++ velgerguideClient/src/app/bar/bar.component.ts
@@ -1,46 +1,48 @@
-import {Component, OnInit} from '@angular/core';
-import { ChartOptions, ChartType} from 'chart.js';
-import {MultiDataSet, Label, Color} from 'ng2-charts';
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import {Color, Label} from 'ng2-charts';
 
 /**
  * legge til verdi fra db der det nå er strenger
  */
-const NavnParti1 = 'Frp';
-const NavnParti2 = 'Venstre';
-const NavnParti3 = 'Høyre';
-const NavnParti4 = 'SV';
-const NavnParti5 = 'MDG';
-const NavnParti6 = 'RV';
-const NavnParti7 = 'SP';
-const NavnParti8 = 'AP';
-const NavnParti9 = 'parti 9';
-const NavnParti10 = 'parti 10';
+const NavnParti1 =  'part1';
+const NavnParti2 = 'part2';
+const NavnParti3 = 'part3';
+const NavnParti4 = 'part4';
+const NavnParti5 = 'part5';
+const NavnParti6 = 'part6';
+const NavnParti7 = 'part7';
+const NavnParti8 = 'part8';
+const NavnParti9 = 'part9';
+const NavnParti10 = 'part10';
 /**
  * parti poeng kobles kobles til data fra Database.
  */
-const PoengParti1 = 90;
-const PoengParti2 = 50;
-const PoengParti3 = 60;
-const PoengParti4 = 70;
-const PoengParti5 = 80;
-const PoengParti6 = 40;
-const PoengParti7 = 5;
-const PoengParti8 = 30;
-const PoengParti9 = 10;
-const PoengParti10 = 100;
+const PoengParti1 = 40;
+const PoengParti2 = 30;
+const PoengParti3 = 79;
+const PoengParti4 = 80;
+const PoengParti5 = 100;
+const PoengParti6 = 60;
+const PoengParti7 = 50;
+const PoengParti8 = 25;
+const PoengParti9 = 25;
+const PoengParti10 = 50;
 /**
  * Farge på stolpene
  */
-const colorParti1 = 'red';
-const colorParti2 = 'green';
-const colorParti3 = 'blue';
-const colorParti4 = 'green';
-const colorParti5 = 'green';
-const colorParti6 = 'red';
-const colorParti7 = 'green';
-const colorParti8 = 'red';
-const colorParti9 = 'blue';
-const colorParti10 = 'blue';
+/**
+ * const colorParti1 = 'blue';
+ * *const colorParti2 = 'green';
+ * const colorParti3 = 'blue';
+ * const colorParti4 = 'green';
+ * const colorParti5 = 'green';
+ * const colorParti6 = 'red';
+ * const colorParti7 = 'green';
+ * const colorParti8 = 'red';
+ * const colorParti9 = 'blue';
+ * const colorParti10 = 'blac';
+ */
 
 @Component({
   selector: 'app-bar',
@@ -49,55 +51,24 @@ const colorParti10 = 'blue';
 })
 
 export class BarComponent implements OnInit {
-      barChartType: ChartType = 'bar';
-      barChartPlugins = [];
-      barChartOptions: ChartOptions = {
-      responsive: true,
-      };
-      barChartLabels: Label[] = [
-        'parti poeng',
-        NavnParti1,
-        NavnParti2,
-        NavnParti3,
-        NavnParti4,
-        NavnParti5,
-        NavnParti6,
-        NavnParti7,
-        NavnParti8,
-        NavnParti9,
-        NavnParti10
-      ];
-      barChartData: MultiDataSet = [
-      [0,
-        PoengParti1,
-        PoengParti2,
-        PoengParti3,
-        PoengParti4,
-        PoengParti5,
-        PoengParti6,
-        PoengParti7,
-        PoengParti8,
-        PoengParti9,
-        PoengParti10,
-      ],
-      ];
-  barChartColors: Color[] = [
-    { backgroundColor:
-        ['black',
-          colorParti1,
-          colorParti2,
-          colorParti3,
-          colorParti4,
-          colorParti5,
-          colorParti6,
-          colorParti7,
-          colorParti8,
-          colorParti9,
-          colorParti10,
-        ]
-    },
-  ];
+ public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+ public barChartLabels: Label[] = [NavnParti1, NavnParti2, NavnParti3, NavnParti4,
+   NavnParti5, NavnParti6, NavnParti7, NavnParti8, NavnParti9, NavnParti10];
+ public barChartType: ChartType = 'bar';
+ public barChartLegend = true;
+ public barChartPlugins = [{
+ }];
 
+ public barChartData: ChartDataSets[] = [
+   {data: [ PoengParti1, PoengParti2, PoengParti3, PoengParti4,
+       PoengParti5, PoengParti6, PoengParti7, PoengParti8,
+       PoengParti9, PoengParti10], label: 'Poeng'},
+  ];
+ public barChartColors: Color[] = [
+   {backgroundColor: 'blue'},
+ ];
   constructor() {}
   ngOnInit() {}
 
